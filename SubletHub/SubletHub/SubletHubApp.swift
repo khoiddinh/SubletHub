e3e5d63225eade66097d827dceb0736790b8b87f
Khoi Dinh <khoiddinh@gmail.com>
2025-04-21 00:02:08 -0400

    init

---
+++ SubletHub/SubletHub/SubletHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  SubletHubApp.swift
+//  SubletHub
+//
+//  Created by Khoi Dinh on 4/21/25.
+//
+
+import SwiftUI
+
+@main
+struct SubletHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SubletHub/SubletHubTests/SubletHubTests.swift
@@ -0,0 +1,17 @@
+//
+//  SubletHubTests.swift
+//  SubletHubTests
+//
+//  Created by Khoi Dinh on 4/21/25.
+//
+
+import Testing
+@testable import SubletHub
+
+struct SubletHubTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
