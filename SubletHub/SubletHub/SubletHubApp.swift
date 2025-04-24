2f0dd506aa1c89dfda93c1cf827898678062d536
Khoi Dinh <khoiddinh@gmail.com>
2025-04-24 16:56:11 -0400

    firebase https calls

--- SubletHub/SubletHub/SubletHubApp.swift
+++ SubletHub/SubletHub/SubletHubApp.swift
@@ -9,11 +9,11 @@ import SwiftUI
 import FirebaseCore
 
 class AppDelegate: NSObject, UIApplicationDelegate {
-  func application(_ application: UIApplication,
+    func application(_ application: UIApplication,
                    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-    FirebaseApp.configure()
-    return true
-  }
+        FirebaseApp.configure()
+        return true
+    }
 }
 
 @main
--- SubletHub/SubletHub/ViewModels/ListingViewModel.swift
+++ SubletHub/SubletHub/ViewModels/ListingViewModel.swift
@@ -6,40 +6,73 @@
 //
 import SwiftUI
 import Foundation
-import FirebaseFirestore
+import CoreLocation
 import Observation
 
 @Observable
 class ListingViewModel {
+    let PROJECT_ID: String = "sublet-hub-52e99"
     var listings: [Listing] = []
-    
-    private let db = Firestore.firestore()
-    private var listener: ListenerRegistration?
 
-    private var listenerRegistration: ListenerRegistration?
-
-    deinit {
-        unregister()
-    }
-
-    func unregister() {
-        if listenerRegistration != nil {
-            listenerRegistration?.remove()
-        }
-    }
-    
-    
     func fetchData() {
-        unregister()
-        listenerRegistration = db.collection("listings").addSnapshotListener { (querySnapshot, error) in
-          guard let documents = querySnapshot?.documents else {
-              print("No documents")
-              return
-          }
-
-          self.listings = documents.compactMap { queryDocumentSnapshot -> Listing? in
-              return try? queryDocumentSnapshot.data(as: Listing.self)
-          }
+        guard let url = URL(string: "https://us-central1-\(PROJECT_ID).cloudfunctions.net/getListings") else {
+            print("Invalid URL")
+            return
         }
+
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            if let data = data {
+                do {
+                    let decodedListings = try JSONDecoder().decode([Listing].self, from: data)
+                    DispatchQueue.main.async {
+                        self.listings = decodedListings
+                    }
+                } catch {
+                    print("Decoding error: \(error)")
+                }
+            } else if let error = error {
+                print("Network error: \(error)")
+            }
+        }.resume()
+    }
+    func createListing(_ listing: Listing, completion: @escaping (Result<String, Error>) -> Void) {
+        guard let url = URL(string: "https://us-central1-\(PROJECT_ID).cloudfunctions.net/createListing") else {
+            completion(.failure(URLError(.badURL)))
+            return
+        }
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+
+        do {
+            let jsonData = try JSONEncoder().encode(listing)
+            request.httpBody = jsonData
+        } catch {
+            completion(.failure(error))
+            return
+        }
+
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            if let error = error {
+                completion(.failure(error))
+                return
+            }
+
+            guard let data = data else {
+                completion(.failure(URLError(.badServerResponse)))
+                return
+            }
+
+            do {
+                let response = try JSONDecoder().decode([String: String].self, from: data)
+                if let id = response["id"] {
+                    completion(.success(id))
+                } else {
+                    completion(.failure(URLError(.cannotParseResponse)))
+                }
+            } catch {
+                completion(.failure(error))
+            }
+        }.resume()
     }
 }
